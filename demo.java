7aaef895d140d4a55a62bb7b5e7eb72a9d56e32a
divya <vdivyashree1@gmail.com>
2023-05-02 18:05:24 +0530

    5 times print

--- demo.java
+++ demo.java
@@ -3,5 +3,11 @@ class hello
 	public static void main (String [] args)
 	{
 		System.out.println("hello java");
+		 System.out.println("hello java");
+		  System.out.println("hello java");
+		   System.out.println("hello java");
+		    System.out.println("hello java");
 	}
 }
+
+clear
